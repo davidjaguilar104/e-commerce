cf72c0cb4d63111c671833f78fd0699b44f01700
davidjaguilar104 <davidjaguilar104@gmail.com>
2021-12-14 15:59:47 -0600

    implemented find one product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -32,6 +32,28 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "product_name", "price", "stock", "category_id"],
+    include: [
+      {
+        model: Category,
+        attributes: ["id", "category_name"],
+      },
+      {
+        model: Tag,
+        as: 'product_tags',
+        attributes: ["id", "tag_name"],
+      },
+    ],
+  })
+    .then((dbProductData) => res.json(dbProductData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // create new product
